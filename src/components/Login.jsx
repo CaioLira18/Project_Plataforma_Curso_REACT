1ecfe08499b4a6066bc2034b10910e9665bb1af6
CaioLira18 <caiozao1212@gmail.com>
2025-03-23 14:19:15 -0300

    Update URL

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,7 +5,7 @@ import Axios from "axios";
 import Profile from "./Profile";
 
 // URL da API para o Railway
-const API_URL = "https://testserver-production-eca2.up.railway.app/";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
 
 const Login = () => {
   const [user, setUser] = useState({
