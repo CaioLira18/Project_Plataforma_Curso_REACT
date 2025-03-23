5be3a4d7f51cb9705547ade84bea65455d9d9bda
CaioLira18 <caiozao1212@gmail.com>
2025-03-22 21:57:55 -0300

    add depedencies

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mysql = require("mysql2");
 const bcrypt = require("bcrypt");
-const cors = require("cors");
+const cors = require(" ");
 require("dotenv").config();
 
 const app = express();
